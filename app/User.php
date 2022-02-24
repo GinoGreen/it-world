164b773a05c161a6ae10c1c7f3437e4e47a25d4f
GinoGreen <luigiverde97@gmail.com>
2022-02-24 16:11:44 +0100

    relationship PIVOT reviews and users

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'region', 'avatar_path', 'cv_path', 'level', 'surename'
     ];
 
     /**
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function reviews() {
+        return $this->hasMany('App\Review');
+    }
 }
