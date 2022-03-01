e454d648f12c1abf625b5bd1954aae695dab36a8
GinoGreen <luigiverde97@gmail.com>
2022-03-01 16:04:11 +0100

    registration field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,124 +5,143 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Registrati per offrire i tuoi servizi') }}</div>
+                <div class="card-header">Registrati per offrire i tuoi servizi</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Nome</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="name"
+                                    type="text" 
+                                    class="form-control 
+                                    @error('name') 
+                                        is-invalid 
+                                    @enderror" 
+                                    name="name" 
+                                    value="{{ old('name') }}" 
+                                    autofocus
+                                >
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('surname') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Cognome</label>
 
                             <div class="col-md-6">
-                                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
-
-                                @error('surname')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="surname"
+                                    type="text" 
+                                    class="form-control 
+                                    @error('surname') 
+                                        is-invalid 
+                                    @enderror" 
+                                    name="surname" 
+                                    value="{{ old('surname') }}"
+                                >
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="region" class="col-md-4 col-form-label text-md-right">{{ __('region') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
 
                             <div class="col-md-6">
+                                <input id="email"
+                                    type="email" 
+                                    class="form-control 
+                                    @error('email') 
+                                        is-invalid 
+                                    @enderror" 
+                                    name="email" 
+                                    value="{{ old('email') }}"
+                                >
+                            </div>
+                        </div>
 
+                        <div class="form-group row">
+                            <label for="region" class="col-md-4 col-form-label text-md-right">Locazione Ufficio</label>
+
+                            <div class="col-md-6">
                                 <select
-                                    name="region" id="region"
-                                    class="form-control">
-                                    <option value="">Selezionare una regione</option>
+                                    name="region" 
+                                    id="region"
+                                    class="form-control"
+                                >
+                                    <option value="">Da dove lavori?</option>
     
                                     @foreach ($regions as $region)
-                                    <option
-                                        @if($region == old('region')) selected  @endif
-                                    value="{{ $region }}">{{ $region }}</option>
+                                        <option
+                                            @if($region == old('region'))
+                                                selected
+                                            @endif
+                                            value="{{ $region }}"
+                                        >{{ $region }}</option>
                                     @endforeach
-    
                                 </select>
-
                             </div>
-
                         </div>
 
                         <div class="form-group row">
-                            <label for="level" class="col-md-4 col-form-label text-md-right">{{ __('level') }}</label>
+                            <label for="level" class="col-md-4 col-form-label text-md-right">Livello di esperienza</label>
 
                             <div class="col-md-6">
-
                                 <select
-                                    name="level" id="level"
-                                    class="form-control">
-                                    <option value="">Selezionare un livello di esperienza</option>
+                                    name="level" 
+                                    id="level"
+                                    class="form-control"
+                                >
+                                    <option value="">Come ti qualifichi?</option>
     
                                     @foreach ($levels as $level)
-                                    <option
-                                        @if($level == old('level')) selected  @endif
-                                    value="{{ $level }}">{{ $level }}</option>
+                                        <option
+                                            @if($level == old('level'))
+                                                selected
+                                            @endif
+                                            value="{{ $level }}"
+                                        >{{ $level }}</option>
                                     @endforeach
-    
                                 </select>
-
                             </div>
 
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="password" 
+                                    type="password" 
+                                    class="form-control 
+                                    @error('password') 
+                                        is-invalid 
+                                    @enderror" 
+                                    name="password" 
+                                    required 
+                                    autocomplete="new-password"
+                                >
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Conferma password</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="password-confirm" 
+                                    type="password" 
+                                    class="form-control" 
+                                    name="password_confirmation" 
+                                    required autocomplete="new-password"
+                                >
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
+                        {{-- submit --}}
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    Registrati
                                 </button>
                             </div>
                         </div>
