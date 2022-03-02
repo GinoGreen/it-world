2949fddff9a59f5e0145a1bbd5f43f91ade4a073
GinoGreen <luigiverde97@gmail.com>
2022-03-02 17:44:30 +0100

    fixing

--- resources/views/admin/partials/dashboard.blade.php
+++ resources/views/admin/partials/dashboard.blade.php
@@ -12,12 +12,12 @@
       <li class="it-dashboard-item">
           <a href="{{ route('admin.review') }}"><span>My Reviews</span></a>
       </li>
-      <li class="it-dashboard-item">
+      {{-- <li class="it-dashboard-item">
           <a href="{{ route('admin.statistic') }}"><span>Statistics</span></a>
       </li>
       <li class="it-dashboard-item">
           <a href="{{ route('chartjs.index') }}"><span>Chartjs</span></a>
-      </li>
+      </li> --}}
       <li class="it-dashboard-item">
           <form onsubmit="return confirm(`Confermi l'eliminazione del profilo?`)"
           action="{{ route('admin.profile.destroy', $profile)}}" method="POST">
