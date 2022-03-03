f9b4aecff6a07eb253d410302e7b76030780f6e8
GinoGreen <luigiverde97@gmail.com>
2022-03-03 17:13:25 +0100

    api search job_role

--- app/Http/Controllers/Api/Job_roleController.php
+++ app/Http/Controllers/Api/Job_roleController.php
@@ -8,8 +8,17 @@ use Illuminate\Http\Request;
 
 class Job_roleController extends Controller
 {
-    public function index(){
+    public function index() {
+
         $job_roles = Job_role::all();
+
+        return response()->json($job_roles);
+    }
+
+    public function search($job_role) {
+
+        $job_roles = Job_role::where('name', 'like', '%' . $job_role . '%')->get();
+
         return response()->json($job_roles);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::namespace('Api')
     ->prefix('job_roles')
     ->group(function(){
         Route::get('/','Job_roleController@index');
-       
+        Route::get('/{name}','Job_roleController@search');
     });
