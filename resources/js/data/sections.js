4574dac69b60e733f6ac3ba9b32fa01054c8b8f0
GinoGreen <luigiverde97@gmail.com>
2022-03-06 15:51:49 +0100

    uncomment

--- resources/js/data/sections.js
+++ resources/js/data/sections.js
@@ -1,55 +1,55 @@
 export default [
-   // {
-   //    title: 'IL PROTALE DI PROFESSIONISTI IT',
-   //    subtitle: 'Cerca Tutti i professionisti del settore a portata di mano',
-   //    class: 'search',
-   //    image: '/img/search.svg',
-   //    col: {
-   //       sx: 9,
-   //       dx: 3,
-   //    }
-   // },
-   // {
-   //    title: 'SCEGLI TRA LE CATEGORIE',
-   //    subtitle: 'Scegli tra le nostre categorie e trova il professionista che fa per te.',
-   //    class: 'categories',
-   //    image: null,
-   //    col: {
-   //       sx: 12,
-   //       dx: 0,
-   //    }
-   // },
-   // {
-   //    title: 'IN EVIDENZA',
-   //    subtitle: 'Fatti consigliare da noi',
-   //    class: 'professionals',
-   //    image: null,
-   //    col: {
-   //       sx: 12,
-   //       dx: 0,
-   //    }
-   // },
-   // {
-   //    title: 'CHI SIAMO',
-   //    subtitle: 'il portale più diffuso in ambito business, visitato ogni giorno da migliaia di utenti, professionisti ed HR manager',
-   //    class: 'about',
-   //    image: '/img/about.svg',
-   //    text: 'IT-World è il marketplace per servizi professionali leader in Italia, e in espansione a livello internazionale. Ti permette di trovare esattamente quello che stai cercando in modo semplice e affidabile. IT-World nasce dall\'idea di un gruppo di imprenditori di successo con esperienza nel mondo del web, che hanno deciso di creare uno strumento in grado di aiutare l’utente nella ricerca del miglior sviluppatore o designer nella propria città, facilitando il contatto tra professionista e cliente, e semplificando il processo di prenotazione.',
-   //    col: {
-   //       sx: 8,
-   //       dx: 4,
-   //    }
-   // },
-   // {
-   //    title: 'UNISCITI ALLA RETE',
-   //    subtitle: 'IT-World è il tuo parter per trovare nuovi clienti online',
-   //    class: 'join',
-   //    image: '/img/join.svg',
-   //    col: {
-   //       sx: 9,
-   //       dx: 3,
-   //    }
-   // },
+   {
+      title: 'IL PROTALE DI PROFESSIONISTI IT',
+      subtitle: 'Cerca Tutti i professionisti del settore a portata di mano',
+      class: 'search',
+      image: '/img/search.svg',
+      col: {
+         sx: 9,
+         dx: 3,
+      }
+   },
+   {
+      title: 'SCEGLI TRA LE CATEGORIE',
+      subtitle: 'Scegli tra le nostre categorie e trova il professionista che fa per te.',
+      class: 'categories',
+      image: null,
+      col: {
+         sx: 12,
+         dx: 0,
+      }
+   },
+   {
+      title: 'IN EVIDENZA',
+      subtitle: 'Fatti consigliare da noi',
+      class: 'professionals',
+      image: null,
+      col: {
+         sx: 12,
+         dx: 0,
+      }
+   },
+   {
+      title: 'CHI SIAMO',
+      subtitle: 'il portale più diffuso in ambito business, visitato ogni giorno da migliaia di utenti, professionisti ed HR manager',
+      class: 'about',
+      image: '/img/about.svg',
+      text: 'IT-World è il marketplace per servizi professionali leader in Italia, e in espansione a livello internazionale. Ti permette di trovare esattamente quello che stai cercando in modo semplice e affidabile. IT-World nasce dall\'idea di un gruppo di imprenditori di successo con esperienza nel mondo del web, che hanno deciso di creare uno strumento in grado di aiutare l’utente nella ricerca del miglior sviluppatore o designer nella propria città, facilitando il contatto tra professionista e cliente, e semplificando il processo di prenotazione.',
+      col: {
+         sx: 8,
+         dx: 4,
+      }
+   },
+   {
+      title: 'UNISCITI ALLA RETE',
+      subtitle: 'IT-World è il tuo parter per trovare nuovi clienti online',
+      class: 'join',
+      image: '/img/join.svg',
+      col: {
+         sx: 9,
+         dx: 3,
+      }
+   },
    {
       title: 'CONTATTACI',
       subtitle: 'Non importa in quale fase del processo ti trovi, siamo qui per aiutarti. Facci sapere cosa hai in mente.',
