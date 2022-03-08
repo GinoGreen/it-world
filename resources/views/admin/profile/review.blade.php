0965eb525f2ccaa74d9e8244a4d32c39ebe64fd1
andreafontaneto <andrea.fontaneto@gmail.com>
2022-03-08 12:32:57 +0100

    reviews vote stars + colors

--- resources/views/admin/profile/review.blade.php
+++ resources/views/admin/profile/review.blade.php
@@ -2,38 +2,45 @@
 
 @section('content')
 <div class="container">
-  <div class="white-wrap-rvu">
-
-      <div class="row">
-
+  <div class="it-reviews-wrapper mt-5">
+    <div class="row">
+      
+      @foreach ($reviews as $review)
+      <div class="col-12 d-flex justify-content-between align-items-center">
         
-        @foreach ($reviews as $review)
-        <div class="col-12 d-flex justify-content-between align-items-center">
-        
-          <div class="circle-avatar-rvu"></div>
-          
-          <div class="wrap-secondary-rvu mb-4">
-            <div >
+        <div class="it-reviews-box mb-4">
+          <div class="it-r-top">
+            <div class="it-r-title">
                 {{$review->title}}
             </div>
-            <div >
+            <div class="it-r-vote">
+                
+                @for ($star = 0; $star < $review->vote; $star++ )
+                  <span class="star">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </span>
+                @endfor
+
+            </div>
+          </div>
+          <div class="it-r-bottom">
+            <div class="it-r-description pt-3">
                 {{$review->description}}
             </div>
-            <div >
-                {{$review->vote}}
-            </div>
-            <div >
+            <div class="it-r-date">
                 {{$review->created_at}}
             </div>
-          </div> 
-       
-        </div>
-          @endforeach   
+          </div>
+        </div> 
+      
       </div>
+      @endforeach  
 
-      <div class="paginate">        
-        {{$reviews->links()}}
-      </div>
+    </div>
+
+    <div class="paginate">        
+      {{$reviews->links()}}
+    </div>
 
   </div>
 </div>
