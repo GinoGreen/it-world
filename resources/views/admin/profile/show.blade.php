b73260dca1e86dc9f8929abf023b9e2c7a85e182
GinoGreen <luigiverde97@gmail.com>
2022-03-08 15:08:16 +0100

    show refactoring

--- resources/views/admin/profile/show.blade.php
+++ resources/views/admin/profile/show.blade.php
@@ -4,33 +4,7 @@
 
 <div class="white-wrapper-show">
 
-   <div class="avatar-circle-show"></div>
-
-   <div class="data-wrapper-show">
-      
-      {{-- @dd($profile->job_roles) --}}
-      @foreach ($profile->job_roles as $job_role)
-         <div class="d-inline">{{$job_role->name}}</div>
-      @endforeach
-
-      @foreach ($fields as $field )
-
-         <div class="container m-1 wrap-text-show">
-
-            <div class="d-inline">{{$field}}</div>
-
-            @if ($profile->$field === null)
-               <div class="d-inline ">{{$field}} non presente</div>
-            @else
-               <div class="d-inline">{{$profile->$field}}</div>
-            @endif
-
-         </div>
-
-      @endforeach
-      
-   </div>
-
+   <h1>{{ Auth::user()->name }}</h1>
 
 </div>
 @endsection
