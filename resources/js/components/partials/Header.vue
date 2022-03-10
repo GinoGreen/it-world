946f3414d75d1a2aa5fa469833fd4f99d27b7a8c
andreafontaneto <andrea.fontaneto@gmail.com>
2022-03-10 19:24:03 +0100

    header fix + hamburger menu

--- resources/js/components/partials/Header.vue
+++ resources/js/components/partials/Header.vue
@@ -12,18 +12,38 @@
 
             <div class="menu h-100">
 
-               <span>Chi siamo</span>
-               <span>
+               <span class="d-none d-md-block">
                   <a href="/login">Accedi</a>
                </span>
-               <span>
+               <span class="d-none d-md-block">
                   <a href="/register">Registrati</a>
                </span>
              
 
-               <div class="wrap-line-menu">
+               <!-- <div class="wrap-line-menu">
                   <div class="menu-line upper"></div>
                   <div class="menu-line lower"></div>
+               </div> -->
+
+               <div id="sidemenu" class="d-block d-md-none">
+                  <button class="sidemenu__btn" v-on:click="navOpen=!navOpen" v-bind:class="{active:navOpen}">
+                     <span class="top"></span>
+                     <span class="mid"></span>
+                     <span class="bottom"></span>
+                  </button>
+                  <transition name="translateX">
+                     <nav v-show="navOpen">
+                        <div class="sidemenu__wrapper">
+                           <ul class="sidemenu__list">
+                              <li class="sidemenu__item"><a href="/login">ACCEDI</a></li>
+                              <li class="sidemenu__item"><a href="/register">REGISTRATI</a></li>
+                              <!-- <li class="sidemenu__item"><a href="">CONTATTACI</a></li> -->
+                              <!-- <li class="sidemenu__item"><a href="">Work</a></li>
+                              <li class="sidemenu__item"><a href="">Link</a></li> -->
+                           </ul>
+                        </div>
+                     </nav>
+                  </transition>
                </div>
 
             </div>
@@ -36,8 +56,20 @@
 
 <script>
 export default {
-   name: 'Header'
+   name: 'Header',
+
+   data(){
+      return {
+         navOpen: false
+      }
+   },
+
+   methods: {
+
+   }
 }
+
+
 </script>
 
 <style lang="scss" scoped>
@@ -68,6 +100,7 @@ export default {
             color: white;
             margin: 0px 20px 7px 0px;
          }
+
          .wrap-line-menu {
             cursor: pointer;
             &:hover {
@@ -88,8 +121,138 @@ export default {
             }
             
          }
+
+         #sidemenu {
+            nav {
+               width: 100%;
+               height: 100%;
+               background: rgb(7,33,66);
+               background: linear-gradient(90deg, rgba(7,33,66,1) 0%, rgba(154,47,118,1) 50%, rgba(255,77,90,1) 100%);
+               position: fixed;
+               top: 0;
+               right: 0;
+               z-index: 99;
+               // box-shadow: 2px 0 3px$grey-6;
+               // overflow-y: scroll;
+            }
+
+            .sidemenu {
+               &__btn {
+                  display: block;
+                  width: 50px;
+                  height: 50px;
+                  background: transparent;
+                  border: none;
+                  position: relative;
+                  z-index: 100;
+                  appearance: none;
+                  cursor: pointer;
+                  outline: none;
+
+                  span {
+                     display: block;
+                     width: 40px;
+                     height: 3px;
+                     margin: auto;
+                     background: white;
+                     position: absolute;
+                     top: 0;
+                     bottom: 0;
+                     left: 0;
+                     right: 0;
+                     transition: all .4s ease;
+
+                     &.top {
+                        transform: translateY(-8px);
+                     }
+
+                     &.bottom {
+                        transform: translateY(8px);
+                     }
+                  }
+                  &.active{
+                     .top {
+                        transform: rotate(-45deg);
+                     }
+                     .mid{
+                        transform: translateX(-20px) rotate(360deg);
+                        opacity: 0;
+                     }
+                     .bottom {
+                        transform: rotate(45deg);
+                     }
+                  }
+
+               }
+
+               &__wrapper {
+               padding-top: 80px;
+            }
+
+               &__list {
+                  padding-top: 50px;
+               list-style:none;
+               padding: 0;
+               margin: 0;
+               }
+
+               &__item {
+                  text-align: center;
+
+                  a {
+                     text-decoration: none;
+                     line-height: 1.6em;
+                     font-size: 30px;
+                     font-weight: 800;
+                     padding: 50px;
+                     display: block;
+                     color: white;
+                     transition: .4s ease;
+
+                     &:hover {
+                        background: rgba(255, 255, 255, 0.2);
+                        color: $secondary_color;
+                     }
+                  }
+               }
+            }
+         }
+
+         .translateX-enter{
+            transform:translateX(-200px);
+            opacity: 0;
+         }
+
+         .translateX-enter-active,.translateX-leave-active{
+            transform-origin: top left 0;
+            transition:.2s ease;
+         }
+
+         .translateX-leave-to{
+            transform: translateX(-200px);
+            opacity: 0;
+         }
       }
 
    }
+
+@media screen and (max-width: 992px){
+
+   header{
+      display: flex;
+      justify-content: space-between;
+   }
+
+}
+
+@media screen and (max-width: 500px){
+
+   header{
+      display: flex;
+      justify-content: space-between;
+      
+   }
+
+}
 
 </style>
