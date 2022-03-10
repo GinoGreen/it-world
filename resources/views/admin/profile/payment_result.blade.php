83b7298f6bc206282b35fddd925a073467b0ad1a
GinoGreen <luigiverde97@gmail.com>
2022-03-10 22:16:08 +0100

    last

--- resources/views/admin/profile/payment_result.blade.php
+++ resources/views/admin/profile/payment_result.blade.php
@@ -5,6 +5,12 @@
 
    <section class="it-section-page flex-column justify-content-start row">
       <h1>{{$data['message']}}</h1>
+      @if ($result->success)
+         
+         <p>pagamento: {{ $result->transaction->amount }}</p>
+         {{-- qui dentro sono tutti i dati di result --}}
+         @dump($result->transaction)
+      @endif
       <a href="{{ route('admin.home') }}">
          <div class="it-btn">
             <button>Torna alla dashboard</button>
